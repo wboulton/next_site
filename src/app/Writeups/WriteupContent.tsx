6e427d13673456ef317108ef69594399c1cc7807
wboulton <wboulton@purdue.edu>
2025-02-18 15:01:32 -0500

    Images!
    images in writeups and other tab now work

--- src/app/Writeups/WriteupContent.tsx
+++ src/app/Writeups/WriteupContent.tsx
@@ -4,12 +4,24 @@ import path from 'path';
 import ReactMarkdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
 
+const customRenderers = {
+  img: (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
+    return (
+      <img
+        src={`/Writeups/${props.src}`}
+        alt={props.alt || ''}
+        style={{ width: 'auto', height: 'auto', margin: '10px' }}
+      />
+    );
+  },
+};
+
 export default async function writeupRender(writeup: string) {
   const filePath = path.join(process.cwd(), 'public', 'writeups', `${writeup}.md`);
   const markdown = await fs.readFile(filePath, 'utf8');
 
   return (
-    <ReactMarkdown remarkPlugins={[remarkGfm]}>
+    <ReactMarkdown remarkPlugins={[remarkGfm]} components={customRenderers}>
       {markdown}
     </ReactMarkdown>
   );
