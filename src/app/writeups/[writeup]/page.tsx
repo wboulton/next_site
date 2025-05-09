d7ae572b810c954addfd92fbde90a4b9cca01f59
William Boulton <wboulton@purdue.edu>
2025-05-09 09:51:25 -0400

    writeup list change

--- src/app/writeups/[writeup]/page.tsx
+++ src/app/writeups/[writeup]/page.tsx
@@ -1,17 +1,17 @@
 import { redirect } from "next/navigation";
 interface WriteupProps {
-  params: Promise<{ title: string }>;
+  params: Promise<{ writeup: string }>;
 }
 const writeups = [
-  { title: 'deadface2024', route: '/DaCube' },
-  { title: 'UC2025', route: '/calling_convention' },
-  { title: 'umd2025', route: '/cmsc351'}
+  { writeup: 'deadface2024', route: '/DaCube' },
+  { writeup: 'UC2025', route: '/calling_convention' },
+  { writeup: 'umd2025', route: '/cmsc351'}
 ];
 
 export default async function StoryPage({ params }: WriteupProps) {
   const resolvedParams = await params;
   console.log(resolvedParams);
-  const writeup = writeups.find((writeup) => writeup.title === resolvedParams.title);
+  const writeup = writeups.find((writeup) => writeup.writeup === resolvedParams.writeup);
 
   if (!writeup) {
     return <div>404 writeup not found</div>;
