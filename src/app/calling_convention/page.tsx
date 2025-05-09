2127e6eec4b63b964a0a029ce8c906aa2ff4a0c8
wboulton <wboulton@purdue.edu>
2025-05-09 11:09:13 -0400

    this might break everything

--- src/app/calling_convention/page.tsx
+++ src/app/calling_convention/page.tsx
@@ -2,6 +2,8 @@ import fs from 'fs/promises';
 import path from 'path';
 import Markdown from 'react-markdown';
 import remarkGfm from 'remark-gfm';
+import { Prism as SyntaxHighlighter } from 'react-syntax-highlighter';
+import { dark } from 'react-syntax-highlighter/dist/esm/styles/c';
 
 const customRenderers = {
   img: (props: React.ImgHTMLAttributes<HTMLImageElement>) => {
@@ -20,7 +22,29 @@ export default async function writeupRender() {
   const markdown = await fs.readFile(filePath, 'utf8');
 
   return (
-    <Markdown remarkPlugins={[remarkGfm]} components={customRenderers}>
+    <Markdown
+      remarkPlugins={[remarkGfm]}
+      components={{
+        ...customRenderers,
+        code({ node, inline, className, children, ...props }) {
+          const match = /language-(\w+)/.exec(className || '');
+          return !inline && match ? (
+            <SyntaxHighlighter
+              {...props}
+              PreTag="div"
+              language={match[1]}
+              style={dark}
+            >
+              {String(children).replace(/\n$/, '')}
+            </SyntaxHighlighter>
+          ) : (
+            <code {...props} className={className}>
+              {children}
+            </code>
+          );
+        },
+      }}
+    >
       {markdown}
     </Markdown>
   );
